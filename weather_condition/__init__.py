ecf1f310a201413a16ad1ff84e30806ffb6a03ab
Marius Kalder <147047923+MKalder@users.noreply.github.com>
2025-02-21 14:31:54 +0700

    update version

--- weather_condition/__init__.py
+++ weather_condition/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.0.8"
-__date__ = "2025-02-21"
+__version__ = "1.0.9"
+__date__ = "2025-02-22"
 __author__ = "Marius Kalder"
 __email__ = "aiweatherchatbot@mariuskalder.io"
 __status__ = "Production"
